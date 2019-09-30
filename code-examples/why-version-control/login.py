fb04b6494562b37d72f51db3e709276c5d077d23
Mats Lindh <mats@lindh.no>
2019-09-30 12:55:51 +0200

    Initial commit

---
+++ code-examples/why-version-control/login.py
@@ -0,0 +1,26 @@
+class Request:
+    def __init__(self):
+        self.form = {}
+
+
+def abort(c, msg):
+    pass
+
+
+class User:
+    @staticmethod
+    def sign_in(email, password):
+        return False
+
+
+request = Request()
+
+
+valid_user = User.sign_in(
+    request.form.get('email'),
+    request.form.get('password'),
+)
+
+if not valid_user:
+    abort(403, 'Invalid user given')
+
